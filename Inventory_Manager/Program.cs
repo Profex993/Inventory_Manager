f0e5fe76e5b149199ff48bca2df0f853825365e6
Profex993 <68472681+Profex993@users.noreply.github.com>
2025-05-19 19:03:34 +0200

    Add project files.

---
+++ Inventory_Manager/Program.cs
@@ -0,0 +1,14 @@
+using Inventory_Manager.Forms;
+
+namespace Inventory_Manager
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
